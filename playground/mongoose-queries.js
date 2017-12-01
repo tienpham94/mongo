1fcb1fc6397de0f30440e9bbd5c71ae7e604f626
Tien Pham <r3dsky02@yahoo.com.vn>
2017-12-01 03:49:54 +0200

    Add todo by ID

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -3,14 +3,21 @@ const {Todo} = require('./../server/models/todo')
 
 var id = '5a1ea6700676f2734f001341'
 
-Todo.find({
-  _id: id
-}).then((todos) => {
-  console.log("Todos", todos);
-})
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log("Todos", todos);
+// })
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log("Todo", todo);
+// })
 
-Todo.findOne({
-  _id: id
-}).then((todo) => {
-  console.log("Todo", todo);
-})
+Todo.findById(id).then((todo) => {
+  if(!todo){
+    return console.log("Id not found");
+  }
+  console.log("Todo by Id", todo);
+}).catch((e) => console.log(e);)
